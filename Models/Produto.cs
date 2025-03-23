ae73c98dbb1cf8da97ae9c8774a6442c2549df2b
Willian <willian.oliveira190@etec.sp.gov.br>
2025-03-22 22:03:41 -0300

    Add models e troca da tela

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
-    class Produto
+    public class Produto
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
